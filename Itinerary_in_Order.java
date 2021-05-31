b1ef8d5e1afa5154d6a108bea0e7028b964faae5
Azeemuddin47 <60206900+Azeemuddin47@users.noreply.github.com>
2021-05-31 09:12:18 -0700

    Add files via upload

---
+++ Itinerary_in_Order.java
@@ -0,0 +1,47 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+public class Source37 {
+
+    public static void findItinerary(Map<String, String> tickets) {
+        Map<String, String> reverseMap = new HashMap<String, String>();
+  
+        for (Map.Entry<String,String> entry: tickets.entrySet())
+            reverseMap.put(entry.getValue(), entry.getKey());
+  
+        String start = null;
+        for (Map.Entry<String,String> entry: tickets.entrySet())
+        {
+              if (!reverseMap.containsKey(entry.getKey()))
+              {
+                   start = entry.getKey();
+                   break;
+              }
+        }
+  
+        
+        if (start == null)
+        {
+           System.out.println("Invalid Input");
+           return;
+        }
+  
+        String to = tickets.get(start);
+        while (to != null)
+        {
+            System.out.println(start +  "->" + to + ", ");
+            start = to;
+            to = tickets.get(to);
+        }
+    }
+
+    public static void main(String[] args) {
+        Map<String, String> tickets = new HashMap<String, String>();
+        Scanner sc = new Scanner(System.in);
+        int n  = sc.nextInt();
+        for(int i = 0 ; i < n ; i++){
+            tickets.put(sc.next(),sc.next());
+        }
+        findItinerary(tickets);
+    }
+}
